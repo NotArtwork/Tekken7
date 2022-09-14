c6e21a77c0714375ced5d830bb3fbe0315f1382d
NotArtwork <AntArce3@gmail.com>
2022-09-14 16:39:44 -0400

    2nd push

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-let FighterDiv = document.querySelector('.fighter-grid')
+let fighters = ["bryan", "anna", "king", "bob", "panda","steve", "kazuya", "noctis", "negan", "alisa", "armor-king", "zafina"]
+
+// Recreate the Fighters grid using this function
+
 const fighterUrl = (name) => {
     return `https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png`
 }
 
-const renderFighterCard = (name) => {
-    const card = document.createElement('div')
-    const p = document.createElement('p')
-    const img = document.createElement('img')
-    card.className = card;
-    card.id = name
-    p.textContent = name
-    img.src = fighterUrl(name)
-    card.append(img, p)
-    FighterDiv.append(card)
-}
+let fightersGrid = document.querySelector('#fighters-div')
+console.log('fighters grid', fighters)
 
-let fighters = ['devil-jin', 'anna', 'bob']
-
-fighters.forEach(renderFighterCard)
+fighters.forEach((fighter) => {
+    let div = document.createElement('div')
+    div.classList.add('fighter-thumb')
+    let img = document.createElement('img')
+    img.src = fighterUrl(fighter)
+    img.style.height = '100px'
+    img.style.width = '100px'
+    div.append(img)
+    fightersGrid.append(img)
+})
